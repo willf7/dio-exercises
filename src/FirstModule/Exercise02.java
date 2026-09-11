8162dfca739e0569b5b51b659da8db560891f24c
William Felipe <williamfelipe081@gmail.com>
2026-09-10 21:50:37 -0300

    feat: add Exercise02.java

---
+++ src/FirstModule/Exercise02.java
@@ -0,0 +1,16 @@
+package FirstModule;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of one side of a square:");
+        int size = scanner.nextInt();
+
+        int area = size * size;
+
+        System.out.println("The area of the square is: " + area);
+    }
+}
