8169b85864343d1ee4e80d2b1fdaa12f86b750f7
William Felipe <williamfelipe081@gmail.com>
2026-09-10 22:06:28 -0300

    feat(secondModule): add Exercise01.java

---
+++ src/SecondModule/Exercise01.java
@@ -0,0 +1,19 @@
+package SecondModule;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to generate the multiplication table:");
+        int baseNumber = scanner.nextInt();
+
+        System.out.println("The multiplication table:");
+
+        for (int i = 1; i <= 10; i++) {
+            int multiplicationResult = baseNumber * i;
+            System.out.println(baseNumber + " x " + i  + " = " + multiplicationResult);
+        }
+    }
+}
